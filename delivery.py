0ae4dc4f238df914649f8d82f40e072aa23ab8ae
dlgusdn0629 <dlgusdn0629@gmail.com>
2026-07-09 13:46:38 +0900

    v6: separate free shipping check

--- delivery.py
+++ delivery.py
@@ -1,12 +1,12 @@
 FREE_SHIPPING_LIMIT = 50000
 TEST_ORDER_PRICE = 50000
 
+def is_free_shipping(order_price):
+    return order_price >= FREE_SHIPPING_LIMIT
+
 def get_delivery_message(order_price):
-    message = "DELIVERY_FEE_REQUIRED"
-
-    if order_price >= FREE_SHIPPING_LIMIT:
-        message = "FREE_SHIPPING"
-
-    return message
+    if is_free_shipping(order_price):
+        return "FREE_SHIPPING"
+    return "DELIVERY_FEE_REQUIRED"
 
 print(get_delivery_message(TEST_ORDER_PRICE))
